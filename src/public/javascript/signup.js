91f70ec1164fdf732c09a60aff96f41752b3fd34
Faisal <faisalalik9@gmail.com>
2021-01-28 16:06:16 +0530

    final toggle

--- src/public/javascript/signup.js
+++ src/public/javascript/signup.js
@@ -1,16 +1,20 @@
 var btn1 = 0;
 var btn2 = 0;
-$('.btn1').click(function(){
-  btn1 = 1;
-  btn2 = 0;
-});
-$('.btn2').click(function(){
-  btn1 = 0;
-  btn2 = 1;
-});
-if (btn1 == 1 && btn2 == 0){
-  $('.btn1').classList.add('active');
-}
-if (btn2 == 1 && btn1 == 0){
-  $('.btn2').classList.add('active');
-}
+ $('.btn1').click(function(){
+   this.classList.add('active');
+   if (btn2 == 1){
+     btn2 = 0;
+     $('button')[1].classList.remove('active');
+   }
+   btn1 = 1;
+ });
+
+ $('.btn2').click(function(){
+
+   this.classList.add('active');
+   if (btn1 == 1){
+     btn1 = 0
+     $('button')[0].classList.remove('active');
+   }
+   btn2 = 1;
+ });

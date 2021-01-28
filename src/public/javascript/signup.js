ca1309d2668bf7c9e05ee22d00d755ad717d0a76
Faisal <faisalalik9@gmail.com>
2021-01-28 15:42:17 +0530

    toggle

--- src/public/javascript/signup.js
+++ src/public/javascript/signup.js
@@ -1,7 +1,16 @@
+var btn1 = 0;
+var btn2 = 0;
 $('.btn1').click(function(){
-  this.classList.add('active');
+  btn1 = 1;
+  btn2 = 0;
 });
-
 $('.btn2').click(function(){
-  this.classList.add("active");
+  btn1 = 0;
+  btn2 = 1;
 });
+if (btn1 == 1 && btn2 == 0){
+  $('.btn1').classList.add('active');
+}
+if (btn2 == 1 && btn1 == 0){
+  $('.btn2').classList.add('active');
+}

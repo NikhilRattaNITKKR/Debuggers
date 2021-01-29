7f5d433f906f533fdae7d76d8a60835ce141fc49
Faisal <faisalalik9@gmail.com>
2021-01-30 00:33:24 +0530

    Added home page sections

---
+++ src/public/javascript/home.js
@@ -0,0 +1,43 @@
+function ScrollAppear(){
+  var x = document.querySelector('.first-block');
+  var height = x.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight/2;
+
+  if (height < screenPosition){
+    x.classList.add('first-block-appear');
+  }
+}
+
+function ScrollAppear2(x){
+  var x = document.querySelector('.second-block');
+  var height = x.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight/2;
+
+  if (height < screenPosition){
+    x.classList.add('second-block-appear');
+  }
+}
+
+function ScrollAppear3(x){
+  var x = document.querySelector('.third-block');
+  var height = x.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight/2;
+
+  if (height < screenPosition){
+    x.classList.add('third-block-appear');
+  }
+}
+
+function ScrollAppear4(x){
+  var x = document.querySelector('.fourth-block');
+  var height = x.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight/2;
+
+  if (height < screenPosition){
+    x.classList.add('fourth-block-appear');
+  }
+}
+window.addEventListener('scroll',ScrollAppear);
+window.addEventListener('scroll',ScrollAppear2);
+window.addEventListener('scroll',ScrollAppear3);
+window.addEventListener('scroll',ScrollAppear4);

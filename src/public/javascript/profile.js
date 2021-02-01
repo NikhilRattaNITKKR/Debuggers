8e00f223b54248514dfd340b4d7772631c5a6db8
Faisal <faisalalik9@gmail.com>
2021-02-01 23:24:08 +0530

    Added Search bar

--- src/public/javascript/profile.js
+++ src/public/javascript/profile.js
@@ -3,3 +3,11 @@ var box = document.querySelector('.post-click');
 button.addEventListener("click", function(){
   box.classList.toggle('post-click-edit');
 });
+
+var search = document.querySelector('.search-bar');
+var search_icon = document.querySelector('#search-icon');
+
+search.addEventListener("focus", function(){
+
+    search_icon.style.opacity = 1;
+});

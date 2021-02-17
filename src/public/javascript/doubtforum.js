d7306523667bdca330a46d5b9d2ea381a51e392c
Faisal <faisalalik9@gmail.com>
2021-02-17 20:19:10 +0530

    Doubt-form almost completed

--- src/public/javascript/doubtforum.js
+++ src/public/javascript/doubtforum.js
@@ -3,3 +3,14 @@ $(".fa-arrows-alt-v").click(function(){
      height: "toggle"
    });
 });
+
+
+$(".new-post-text").click(function(){
+  $(".doubt-section").addClass("doubt-section-none");
+  $(".post-a-doubt").addClass("post-a-doubt-display");
+})
+
+$("#cancel-btn").click(function(){
+  $(".doubt-section").removeClass("doubt-section-none");
+  $(".post-a-doubt").removeClass("post-a-doubt-display");
+})

86ade059138a6e7df5884c527fdb6f3c91576a05
hemantlakhotia204 <hemantlakhotia204@gmail.com>
2021-02-18 18:47:44 +0530

    hemant

--- src/public/javascript/doubtforum.js
+++ src/public/javascript/doubtforum.js
@@ -6,11 +6,34 @@ $(".fa-arrows-alt-v").click(function(){
 
 
 $(".new-post-text").click(function(){
-  $(".doubt-section").addClass("doubt-section-none");
-  $(".post-a-doubt").addClass("post-a-doubt-display");
+
+  setTimeout(function(){
+    $(".doubt-section").addClass("doubt-section-none");
+    $(".post-a-doubt").addClass("post-a-doubt-display");
+  },300);
+
 })
 
 $("#cancel-btn").click(function(){
-  $(".doubt-section").removeClass("doubt-section-none");
-  $(".post-a-doubt").removeClass("post-a-doubt-display");
-})
+  setTimeout(function(){
+    $(".doubt-section").removeClass("doubt-section-none");
+    $(".post-a-doubt").removeClass("post-a-doubt-display");
+  },300);
+
+});
+
+$(".add-a-comment").click(function(){
+  setTimeout(function(){
+    $("#new-comment-div").css("display","flex");
+  },200);
+
+
+});
+
+$("#new-btn-cancel").click(function(){
+  setTimeout(function(){
+      $("#new-comment-div").css("display","none");
+      $(".new-comment-input").val("");
+  },200);
+
+});

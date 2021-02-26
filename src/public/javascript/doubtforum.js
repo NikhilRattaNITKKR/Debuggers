0f410c8813bf44012eff43fc58f8355faa782c75
Faisal <faisalalik9@gmail.com>
2021-02-26 20:10:09 +0530

    added onclick

--- src/public/javascript/doubtforum.js
+++ src/public/javascript/doubtforum.js
@@ -23,36 +23,68 @@ setTimeout(function(){
 
 });
 
-$(".add-a-comment").click(function(){
-setTimeout(function(){
-  $("#new-comment-div").css("display","flex");
-  $(".new-comment-input").focus();
-},200);
+// $(".add-a-comment").click(function(){
+// setTimeout(function(){
+//   $("#new-comment-div").css("display","flex");
+//   $(".new-comment-input").focus();
+// },200);
+//
+//
+// });
+//
+// $("#new-btn-cancel").click(function(){
+// setTimeout(function(){
+//     $("#new-comment-div").css("display","none");
+//     $(".new-comment-input").val("");
+// },200);
+//
+// });
+//
+// $("#answer-trigger").click(function(){
+//   setTimeout(function(){
+//     $("#answer-pop").css("display","block");
+//   },300);
+//   setTimeout(function(){
+//     $("#answer-input").focus();
+//   },600);
+//
+// });
+//
+// $(".cancel-answer").click(function(){
+//   setTimeout(function(){
+//     $("#answer-pop").css("display","none");
+//     $("#answer-input").val("");
+//   },300);
+// });
 
 
-});
+function addComment(){
+  setTimeout(function(){
+    $("#new-comment-div").css("display","flex");
+    $(".new-comment-input").focus();
+  },200);
+}
 
-$("#new-btn-cancel").click(function(){
-setTimeout(function(){
-    $("#new-comment-div").css("display","none");
-    $(".new-comment-input").val("");
-},200);
+function cancelComment(){
+  setTimeout(function(){
+      $("#new-comment-div").css("display","none");
+      $(".new-comment-input").val("");
+  },200);
+}
 
-});
 
-$("#answer-trigger").click(function(){
+function addAnswer(){
   setTimeout(function(){
     $("#answer-pop").css("display","block");
   },300);
   setTimeout(function(){
     $("#answer-input").focus();
   },600);
+}
 
-});
-
-$(".cancel-answer").click(function(){
+function cancelAnswer(){
   setTimeout(function(){
     $("#answer-pop").css("display","none");
     $("#answer-input").val("");
   },300);
-})
+}
